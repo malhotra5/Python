218619e2763fc9f051a612658e001cd01df37cec
malhotra5 <rohitvinodmalhotra@gmail.com>
2018-10-12 15:14:43 -0700

    Initial commit

---
+++ Tutorial #1.py
@@ -0,0 +1,12 @@
+## Tutorial 1
+
+
+## Creating Variables. These store values such as numbers or sentences
+
+string = 'This sentence is also known as string'
+number = 390
+
+## Using print
+
+# You can print numbers or strings
+print()
